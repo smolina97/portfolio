5983f0c017ac3bdfa6c5cf25525e600e20d9ff52
smolina97 <santi.lds17@gmail.com>
2024-04-19 14:33:27 -0500

    about, education, experience y projectos, + fixes al css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [ function({ addComponents }) {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        "desktop": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+
+        "laptop": { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        "l-phone": { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+
+        "m-phone": { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        "s-phone": { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+      },
+      
+      gridTemplateColumns: {
+        'auto-fill': 'repeat(auto-fill, minmax(200px, 1fr))',
+      },
+    },
+  },
+  plugins: [
+    function ({ addComponents }) {
       addComponents({
-        'section': {
-          maxWidth: '700px',
-          margin: '0 auto 72px',
-        }
-      })
-    }
-  ]
-}
+        section: {
+          maxWidth: "700px",
+          margin: "0 auto 72px",
+        },
+      });
+    },
+  ],
+};
